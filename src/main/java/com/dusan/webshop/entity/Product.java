e101b759e996bb1ccfcf35c35c392c6fa8dcc525
dusanstevic93 <dusanstevic93@gmail.com>
2020-06-20 19:58:51 +0200

    add ProductEntity

---
+++ src/main/java/com/dusan/webshop/entity/Product.java
@@ -0,0 +1,43 @@
+package com.dusan.webshop.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+public class Product {
+
+    @Setter(AccessLevel.NONE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_id_sequence")
+    @SequenceGenerator(name = "product_id_sequence", sequenceName = "product_id_sequence", allocationSize = 1)
+    private Long id;
+
+    private String name;
+
+    private BigDecimal price;
+
+    private int quantity;
+
+    private String shortDescription;
+
+    private BigDecimal weight;
+
+    @ElementCollection
+    @CollectionTable(name = "product_image", joinColumns = @JoinColumn(name = "product_id"))
+    @Column(name = "image")
+    private Set<String> images = new HashSet<>();
+
+    @ManyToOne
+    private ProductBrand productBrand;
+
+    @ManyToOne
+    private ProductCategory productCategory;
+}
---
+++ src/test/java/com/dusan/webshop/dao/ProductRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.dusan.webshop.dao;
+
+import com.dusan.webshop.entity.Product;
+import com.dusan.webshop.entity.ProductBrand;
+import com.dusan.webshop.entity.ProductCategory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductCategoryRepository categoryRepository;
+
+    @Autowired
+    private ProductBrandRepository brandRepository;
+
+
+    @Test
+    void testSaveProduct() {
+
+        ProductCategory category = new ProductCategory();
+        category.setName("Phones");
+
+        ProductBrand brand = new ProductBrand();
+        brand.setName("Samsung");
+
+        Product product = new Product();
+        product.setName("A10");
+        product.setPrice(new BigDecimal(350.00));
+        product.setQuantity(15);
+        product.setWeight(new BigDecimal(0.2));
+        product.setShortDescription("Mobile phone");
+        product.getImages().add("image1.jpg");
+        product.getImages().add("image2.jpg");
+        product.setProductBrand(brand);
+        product.setProductCategory(category);
+
+        categoryRepository.save(category);
+        brandRepository.save(brand);
+        productRepository.save(product);
+
+    }
+}
