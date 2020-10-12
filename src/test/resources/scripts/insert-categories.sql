9e2e34d99a03d386fb330ef0817354e3acb01dcb
dusanstevic93 <dusanstevic93@gmail.com>
2020-10-12 02:37:48 +0200

    add database profiles

--- src/test/resources/scripts/insert-categories.sql
+++ src/test/resources/scripts/insert-categories.sql
@@ -1,2 +1,2 @@
-INSERT INTO product_category(id, name, parent_id) VALUES((SELECT nextval('product_category_id_sequence')), 'subcategory 1', 0);
-INSERT INTO product_category(id, name, parent_id) VALUES((SELECT nextval('product_category_id_sequence')), 'subcategory 2', 0);
+INSERT INTO product_category(id, name, parent_id) VALUES(1, 'subcategory 1', 0);
+INSERT INTO product_category(id, name, parent_id) VALUES(2, 'subcategory 2', 0);
